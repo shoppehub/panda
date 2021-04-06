119ed471d1076e4a96e1b1798c0290275ae9f888
jiaoge <jiaoyingjun@youkeda.com>
2021-04-07 00:45:08 +0800

    add site mode

--- model/base.go
+++ model/base.go
@@ -4,50 +4,13 @@ import (
 	"time"
 )
 
-type JsonTime time.Time
-
+// 默认的时间格式
 const JsonTimeLayout = "2006-01-02 15:04:05"
-
-type gmt struct {
-	GmtCreated  *JsonTime `json:"gmtCreated,omitempty"`
-	GmtModified *JsonTime `json:"gmtModified,omitempty"`
-}
-
-// 实现它的json序列化方法
-func (this JsonTime) MarshalJSON() ([]byte, error) {
-	var tt = time.Time(this)
-
-	if tt.IsZero() {
-		return []byte("null"), nil
-	}
-
-	data := make([]byte, 0)
-	data = append(data, '"')
-	data = tt.AppendFormat(data, JsonTimeLayout)
-	data = append(data, '"')
-
-	return data, nil
-}
-
-// 实现它的json反序列化方法
-func (this *JsonTime) UnmarshalJSON(data []byte) error {
-
-	if data == nil {
-		return nil
-	}
-
-	ts, err := time.Parse(`"`+JsonTimeLayout+`"`, string(data))
-
-	//this = time
-	*this = JsonTime(ts)
-
-	return err
-}
 
 // 时间模型，设置开始和结束时间
 type Gmt struct {
-	GmtCreated  JsonTime `json:"gmtCreated,omitempty"`
-	GmtModified JsonTime `json:"gmtModified,omitempty"`
+	GmtCreated  time.Time `json:"gmtCreated,omitempty"`
+	GmtModified time.Time `json:"gmtModified,omitempty"`
 }
 
 // id 为 string 的基础模型
--- service/site.go
+++ service/site.go
@@ -1,7 +1,22 @@
 package service
 
-// 站点模型
+import "github.com/shoppehub/shoppe/model"
+
+// 站点模型,前台和后台公用一个模型
 type Site struct {
+	model.BaseStringId
+	// 二级域名
+	Subdomain string `json:"subdomain,omitempty"`
+	// 顶级域名
+	Domain string `json:"domain,omitempty"`
+	// 用户Id
+	UserId string `json:"userId,omitempty"`
+	// 站点名称
+	Name string `json:"name,omitempty"`
+	// 站点分类
+	CategoryId string `json:"categoryId,omitempty"`
+	// 站点版本
+	Version int `json:"version,omitempty"`
 }
 
 func InitSite() {
